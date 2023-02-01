ab94b1fcf543c8b7781974abecd3f6026832cf72
Ehsan <engtekh@gmail.com>
2023-02-01 20:02:18 +0330

    Delete train.sh

--- train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/students/yiwei/yiwei_gitlab/EffFormer/
-chmod +x train.sh
-/home/students/yiwei/anaconda3/envs/missformer/bin/python /home/students/yiwei/yiwei_gitlab/EffFormer/train_MSTransception.py "$@"
-
-
-
-
